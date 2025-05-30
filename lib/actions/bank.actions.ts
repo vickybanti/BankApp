e4493aecbd762d17c34d24be6f74846029b527b9
VICTOR <olamuyiwavictor55@gmail.com>
2025-05-30 01:45:24 +0100

    addded select

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -118,10 +118,7 @@ export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
     const allTransactions = [...transactions, ...transferTransactions].sort(
       (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
     );
-      const allTransactions = [...transactions, ...transferTransactions].sort(
-      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-    );
-
+      
     return parseStringify({
       data: account,
       transactions: allTransactions,
