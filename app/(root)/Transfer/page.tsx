b853919d0ade26fdc2e8d242864eac8630f99c28
VICTOR <olamuyiwavictor55@gmail.com>
2025-06-01 21:43:23 +0100

    fixd transfer error

--- app/(root)/Transfer/page.tsx
+++ app/(root)/Transfer/page.tsx
@@ -1,30 +1,29 @@
+"use client"
+export const dynamic = "force-dynamic"
+
 import React from 'react'
 import { getLoggedInUser } from '@/lib/actions/user.actions'
 import { getAccounts } from '@/lib/actions/bank.actions'
-import PaymentTransferForm from '@/components/PaymentTransferForm';
-import HeaderBox from '@/components/HeaderBox';
+import PaymentTransferForm from '@/components/PaymentTransferForm'
+import HeaderBox from '@/components/HeaderBox'
 
-
-const Tranfer = async() => {
-  const loggedIn = await getLoggedInUser();
-   const accounts = await getAccounts({ 
-    userId: loggedIn.$id
-  })
+const Transfer = async () => {
+  const loggedIn = await getLoggedInUser()
+  const accounts = await getAccounts({ userId: loggedIn.$id })
   const accountsData = accounts?.data
 
   return (
     <section className="payment-transfer">
-      <HeaderBox 
-      title="Payment Transfer"
-      subtext="Please provide any specific details"
+      <HeaderBox
+        title="Payment Transfer"
+        subtext="Please provide any specific details"
       />
 
       <section className="size-full pt-5">
-        <PaymentTransferForm accounts={accountsData}/>
+        <PaymentTransferForm accounts={accountsData} />
       </section>
-
     </section>
   )
 }
 
-export default Tranfer
+export default Transfer
